413a11fa084918de2b9b625d8f4d1eadb92c311d
Blobby <123762943+Blobby3892@users.noreply.github.com>
2023-03-27 09:34:56 -0400

    Delete scripts directory

--- scripts/AutoRefresh.js
+++
@@ -1,1 +0,0 @@
-javascript:timeout=prompt("Set timeout [s]");current=location.href;if(timeout>0)  setTimeout('reload()',1000*timeout);else  location.replace(current);function reload(){  setTimeout('reload()',1000*timeout);  fr4me='<frameset cols=\'*\'>\n<frame src=\''+current+'\'/>';  fr4me+='</frameset>';  with(document){write(fr4me);void(close())};}
